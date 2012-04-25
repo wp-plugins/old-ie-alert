30b25a63a5bdd7b3c9f8beefb9865bc7985c77f8
mikeyott <mikeyott@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-25 08:12:54 +0000

    First commit for Old IE Alert plugin.
    git-svn-id: https://plugins.svn.wordpress.org/old-ie-alert/trunk@536051 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ old-ie-alert.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Plugin Name: Old IE Alert
+Plugin URI: http://michaelott.id.au
+Description: Display an upgrade nag on IE7 and lower.
+Author: Michael Ott
+Version: 0.5
+Author URI: http://michaelott.id.au
+*/
+
+// Create action and function for header output
+add_action('wp_head', 'oiea_head');
+function oiea_head() { 
+
+$content_head = '
+<!--[if lte IE 7]>
+<link rel="stylesheet" href="' . get_option('home') . "/" . $siteurl . 'wp-content/plugins/' . basename(dirname(__FILE__)) . '/css/oiea.css" type="text/css" media="screen" />
+<![endif]-->
+'; 
+
+  echo $content_head;
+  print "\n";
+  
+}
+
+// Create action and function for footer output
+add_action('wp_footer', 'oiea_foot');
+function oiea_foot() { 
+
+$content_footer = '
+<!--[if lte IE 7]>
+<div id="oiea">
+	<p>
+    	<strong>Oh my! What\'s this about?</strong>
+        Parts of this site may look broken because you are using a very old (and insecure) web browser. But don\'t worry, you can upgrade to any of these for free.
+    </p>
+    <ul>
+		<li title="Chrome" class="chrome"><a href="http://www.google.com/chrome">Chrome</a></li>
+    	<li title="Firefox" class="ff"><a href="http://www.getfirefox.com/">Firefox</a></li>
+        <li title="Safari" class="safari"><a href="http://www.apple.com/safari/download/">Safari</a></li>
+        <li title="Internet Explorer" class="ie"><a href="http://www.microsoft.com/Windows/internet-explorer/">Internet Explorer</a></li>
+    </ul>
+</div>
+<![endif]-->
+'; 
+
+  echo $content_footer;
+  print "\n";
+}
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== OLD IE Alaert ===
+Contributors: mikeyott
+Tags: ie6, ie7, old, browser, internet, explorer
+Requires at least: 3.3.2
+Tested up to: 3.3.2
+Stable tag: trunk
+
+Display an upgrade nag on IE7 and lower.
+
+== Description ==
+
+Display an upgrade nag for people who are still using Internet Explorer 7 or lower. The nag also provides links to download Chrome, Firefox, Safari and Internet Explorer.
+
+== Installation ==
+
+Install, activate, done.
+
+== Uninstall ==
+
+Deactivate the plugin, delete if desired.
+
+== Official Web Site (and support) ==
+
+<a href="http://www.thatwebguyblog.com/post/alert-for-ie7-and-lower-with-this-wordpress-plugin">That Web Guy Blog</a>
+
+== Changelog ==
+
+= 0.5 =
+
+* Cleaned up code.
+
+= 0.4 =
+
+* Replaced PNG with GIF.
+* Fixed function name conflict.
